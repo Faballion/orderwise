4e06e5b1b82b4fe5327a37fc82fd6a6b0f319474
Daniel R. Pearson <faballion@gmail.com>
2019-05-23 08:58:10 +0900

    Add new line when input max length is reached

--- js/script.js
+++ js/script.js
@@ -1,27 +1,47 @@
 $(document).ready(function () {
 
+  const MAX_INPUT_LENGTH = 40
   var lineNumber = 0;
-  var lineValues = [];
+  var lineValues = []; // Hold values for all lines
+  var lineLock = []; // Hold value to determine if line is over 40 characters
 
   // Add new line
-  $("#newLine").on("click", function () {
+  function addNewLine() {
     lineNumber++;
     lineValues.push('');
-    $(".lines").append("<div class='line'>Line " + lineNumber + " <input class='line-input' type='text'/> <button class='removeLine'>X</button></div>");
+    lineLock.push(false);
+    $(".lines").append("<div class='line'>Line " + lineNumber + " <input class='line-input' type='text' maxlength='" + MAX_INPUT_LENGTH + "'/> <button class='removeLine'>X</button></div>");
+  }
+
+  // On button click, add new line
+  $("#newLine").on("click", function () {
+    addNewLine();
   });
 
   // Remove line
   $(document).on("click", ".removeLine", function () {
     var index = $(this).parent().index();
     lineValues.splice(index, 1);
+    lineLock.splice(index, 1);
     $("#outputLines").val(lineValues.join("\n"));
     $(this).closest('.line').remove();
   });
 
-  // Display in textarea
-  $(document).on("keyup", ".line-input", function () {
-    lineValues[$(this).parent().index()] = $(this).val();
-    $("#outputLines").val(lineValues.join("\n"));
+  $(document).on("keyup", ".line-input", function (event) {
+    var inputText = $(this).val();
+    if ((inputText.length) < MAX_INPUT_LENGTH) {
+      lineValues[$(this).parent().index()] = inputText;
+      lineLock[$(this).parent().index()] = false;
+      $("#outputLines").val(lineValues.join("\n")); // Display in textarea
+    } else {
+      if (lineLock[$(this).parent().index()] === false) {
+        lineValues[$(this).parent().index()] = inputText;
+        lineLock[$(this).parent().index()] = true;
+        $("#outputLines").val(lineValues.join("\n"));
+        addNewLine();
+        $(this).parent().siblings(":last").find(".line-input").focus(); // Focus new line
+      }
+    }
   });
 
 });  
