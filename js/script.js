a0f84fae99c00a30276edd69ffdaafa5a720d38b
Daniel R. Pearson <faballion@gmail.com>
2019-05-23 17:57:13 +0900

    Restrict output to 1000 characters

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
 
   const MAX_INPUT_LENGTH = 40
+  const MAX_TEXTAREA_LENGTH = 1000
   var lineNumber = 0;
   var lineValues = []; // Hold values for all lines
-  var lineLock = []; // Hold value to determine if line is over 40 characters
+  var lineLock = []; // Hold boolean to determine if line is over 40 characters
+  var outputLinesLength = 0;
 
   // Add new line
   function addNewLine() {
@@ -23,25 +25,33 @@ $(document).ready(function () {
     var index = $(this).parent().index();
     lineValues.splice(index, 1);
     lineLock.splice(index, 1);
-    $("#outputLines").val(lineValues.join("\n"));
+    $("#outputLines").val(lineValues.join("\n")).change();
     $(this).closest('.line').remove();
   });
 
-  $(document).on("keyup", ".line-input", function (event) {
+  $(document).on("input", ".line-input", function (event) {
     var inputText = $(this).val();
-    if ((inputText.length) < MAX_INPUT_LENGTH) {
+    if ((inputText.length < MAX_INPUT_LENGTH) && (outputLinesLength < MAX_TEXTAREA_LENGTH)) {
       lineValues[$(this).parent().index()] = inputText;
       lineLock[$(this).parent().index()] = false;
-      $("#outputLines").val(lineValues.join("\n")); // Display in textarea
+      $("#outputLines").val(lineValues.join("\n")).change(); // Display in textarea
     } else {
-      if (lineLock[$(this).parent().index()] === false) {
+      if ((lineLock[$(this).parent().index()] === false) && (outputLinesLength < MAX_TEXTAREA_LENGTH)) {
         lineValues[$(this).parent().index()] = inputText;
         lineLock[$(this).parent().index()] = true;
-        $("#outputLines").val(lineValues.join("\n"));
+        $("#outputLines").val(lineValues.join("\n")).change();
         addNewLine();
         $(this).parent().siblings(":last").find(".line-input").focus(); // Focus new line
       }
     }
+  });
+
+
+  $(document).on("change", "#outputLines", function () {
+    // Display length of textarea
+    var maxLength = $(this).attr("maxlength");
+    outputLinesLength = $(this).val().length;
+    $('#outputSubText').text("(" + outputLinesLength + "/" + maxLength + ")");
   });
 
 });  
